ced7b21f7c698df0b6c87031c9f6a8ef2cf11bce
Fiona <fyeung21@gmail.com>
2020-09-15 20:35:49 -0700

    feat: Setup API call and utils

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,27 @@
 import React, { useState } from 'react';
+import { fetchQuizQuestions, Difficulty, QuestionState } from "./API";
+
+//Components
 import { QuestionCard } from './components/QuestionCard';
+
+type AnswerObject = {
+  question: string;
+  answer: string;
+  correct: boolean;
+  correctAnswer: string;
+}
 
 const TOTAL_QUESTIONS = 10;
 
 const App = () => {
   const [loading, setLoading] = useState(false);
-  const [questions, setQuestions] = useState([]);
+  const [questions, setQuestions] = useState<QuestionState[]>([]);
   const [number, setNumber] = useState(0);
-  const [userAnswers, setUserAnswers] = useState([]);
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([]);
   const [score, setScore] = useState(0);
   const [gameOver, setGameOver] = useState(true);
+
+  console.log(fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY))
 
   const startQuiz = async () => {
 
@@ -35,14 +47,14 @@ const App = () => {
 
       <p>Loading Questions...</p>
 
-      <QuestionCard
+      {/* <QuestionCard
         questionNum={number + 1}
         totalQuestions={TOTAL_QUESTIONS}
         question={questions[number].question}
         answers={questions[number].answers}
         userAnswer={userAnswers ? userAnswers[number] : undefined}
         callback={checkAnswer}
-      />
+      /> */}
 
       <button className="next" onClick={nextQuestion}>
         Next Question
