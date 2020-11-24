301faf0a0f596d134ce46c1f7aa8d9167205f04e
Fiona <fyeung21@gmail.com>
2020-11-24 00:47:00 -0800

    Updates

---
+++ precache-manifest.34dbf29c812ffd23cd06f16576aaafef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79d5312390af0b9790624dc03d1e7269",
+    "url": "/quiz-app/index.html"
+  },
+  {
+    "revision": "970e4cdc55671f94beb7",
+    "url": "/quiz-app/static/js/2.96b09680.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/quiz-app/static/js/2.96b09680.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b944fda740b1bce19b34",
+    "url": "/quiz-app/static/js/main.55d24f0d.chunk.js"
+  },
+  {
+    "revision": "e15a1632907c25963bb7",
+    "url": "/quiz-app/static/js/runtime-main.4f483d45.js"
+  },
+  {
+    "revision": "401e10fbd75529dd8087762794a80e10",
+    "url": "/quiz-app/static/media/calmseas.401e10fb.jpg"
+  }
+]);
